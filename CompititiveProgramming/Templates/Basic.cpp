0f87a0cd76de7d424828f0afaffbeb53ff6aff22
Onkar Ratnaparkhi <73252373+cod-OR@users.noreply.github.com>
2021-04-28 23:48:04 +0530

    Create Basic.cpp

---
+++ CompititiveProgramming/Templates/Basic.cpp
@@ -0,0 +1,66 @@
+
+                        //    I solemnly swear that I am upto no good //
+
+#include <bits/stdc++.h>
+using namespace std;
+#define sub             freopen("input.txt", "r", stdin);//freopen("output.txt", "w", stdout);
+#define ll              long long
+#define ld              long double
+#define ttime           cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';
+#define pb              push_back
+#define unq(a)          sort(all(a));(a).resize(unique(all(a)) - (a).begin())
+#define sz(x)           (int)((x).size())
+#define input(arr,n)    for(ll i1=0;i1<n;i1++ )cin>>arr[i1]
+#define fast            ios_base::sync_with_stdio(false);cin.tie(0);
+#define all(x)          (x).begin(),(x).end()
+#define rep(i,a,b)      for(ll i=a;i<b;i++)
+#define PR(x)           cout << #x " = " << (x) << "\n"
+#define mp              make_pair
+#define ff              first
+#define ss              second
+#define YY              cout<<"Yes"<<endl
+#define NN              cout<<"No"<<endl
+#define ppc             __builtin_popcount
+#define ppcll           __builtin_popcountll
+
+const long long INF=1e18;
+const long long A=200005;
+const long long mod=1000000007;
+
+#define endl "\n"
+// #define int ll
+
+typedef pair<ll,ll> pairll;
+typedef map<ll,ll>  mapll;
+typedef map<char,ll> mapch;
+bool comp(ll a,ll b){
+    return a>b;
+}
+
+ll n,k,tt=1;
+void solve(){
+    //string s,t;
+    // ll m,q,x,y;
+    cin>>n; 
+    std::vector<ll> ar(n);
+    input(ar,n);
+    
+
+}
+
+signed main(){ 
+    sub;    
+    fast;
+    // pre();
+    // clock_t clk = clock();
+    ll t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    // ttime;
+    return 0;
+}        
+
+                                // Mischief Managed //   
+                                         

636898ee2727bb2e9b4a4ef4b5e238af16562fc7
Onkar Ratnaparkhi <73252373+cod-OR@users.noreply.github.com>
2021-04-30 14:04:51 +0530

    Update Generator.cpp
    mt 19937 added

--- CompititiveProgramming/Templates/Generator.cpp
+++ CompititiveProgramming/Templates/Generator.cpp
@@ -1,13 +1,19 @@
+mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+
 void solve(){
-    
-    n= rand() % A +1 ;
+	$1
+    n= rng() % A +1 ;
     cout<<n<<endl;
 
     std::vector<ll> ar(n);
     
     for(int i=0;i<n;i++){
-        ar[i]= rand() % A + 1;
+        ar[i]= rng() % A + 1;
     }
+
+    // to creat a permutation
+    // for (int i = 0; i < N; i++)ar[i] = i;
+    // shuffle(all(ar), rng);
 
     for(auto it:ar)cout<<it<<" ";
     cout<<endl;    

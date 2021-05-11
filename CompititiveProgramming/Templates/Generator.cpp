976e26cd02ccd2f2cfbcbd3063787b9c293e9fed
Onkar Ratnaparkhi <73252373+cod-OR@users.noreply.github.com>
2021-05-11 11:54:02 +0530

    Update Generator.cpp

--- CompititiveProgramming/Templates/Generator.cpp
+++ CompititiveProgramming/Templates/Generator.cpp
@@ -1,21 +1,26 @@
 mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
 
 void solve(){
-	$1
-    n= rng() % A +1 ;
-    cout<<n<<endl;
+    freopen("input.txt", "w", stdout);	// Directs the output to file "input.txt"
+    ll t=1;
+    t= rng()%N;         // Testcases  
+    cout<<t<<endl;		
+    while(t--){
+        $1
+        n= rng() % A +1 ;
+        cout<<n<<endl;
 
-    std::vector<ll> ar(n);
-    
-    for(int i=0;i<n;i++){
-        ar[i]= rng() % A + 1;
+        std::vector<ll> ar(n);
+        
+        for(int i=0;i<n;i++){
+            ar[i]= rng() % A + 1;
+        }
+
+        // to creat a permutation
+        // for (int i = 0; i < n; i++)ar[i] = i+1;
+        // shuffle(all(ar), rng);
+
+        for(auto it:ar)cout<<it<<" ";
+        cout<<endl;    
     }
-
-    // to creat a permutation
-    // for (int i = 0; i < N; i++)ar[i] = i;
-    // shuffle(all(ar), rng);
-
-    for(auto it:ar)cout<<it<<" ";
-    cout<<endl;    
-
 }

eb33bcc6e66230f034771513f48c26f8a78ca510
Onkar Ratnaparkhi <73252373+cod-OR@users.noreply.github.com>
2021-07-06 16:25:38 +0530

    Update Merge_Sort.cpp

--- Algorithms/Sorting/Merge_Sort.cpp
+++ Algorithms/Sorting/Merge_Sort.cpp
@@ -1,87 +1,58 @@
-                //    I solemnly swear that I am upto no good //
 
 #include <bits/stdc++.h>
-#define sub   freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
 using namespace std;
 #define ll long long
-#define pb push_back
-#define endl "\n"
-#define input(arr,n) for(ll i1=0;i1<n;i1++ )cin>>arr[i1]
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define mod 1000000007
-#define all(x) x.begin(),x.end()
-#define mp make_pair
-#define ff first
-#define ss second
-#define NN cout<<"NO"<<endl;
-#define YY cout<<"YES"<<endl;
-#define ub upper_bound
-#define lb lower_bound
-#define A 1000001
-#define make(a,b,c) make_pair(a,make_pair(b,c)) 
-bool comp(ll a,ll b){
-        return a>b;
+
+void display(vector<int>& ar){
+    for(auto it:ar){
+        cout<<it<<" ";
+    }
+    cout<<endl;
 }
 
-void merge(std::vector<ll> &ar, ll st1,ll en1,ll st2,ll en2){
-    int i=st1,j=st2;
-    std::vector<ll> v;
-    while(i<=en1 && j<=en2){
-        if(ar[i]<=ar[j]){
-            v.pb(ar[i]);
-            i++;
-        }
-        else{
-            v.pb(ar[j]);
-            j++;
-        }
+void merge(vector<int> & ar, int st, int en){
+    vector<int> v;
+    int m= (st+en)/2;
+    int left=st, right = m;
+    while(left<m && right< en){
+        if(ar[left]<=ar[right])v.push_back(ar[left++]);
+        else    v.push_back(ar[right++]);
+
     }
-    while(j<=en2){
-        
-        v.pb(ar[j]);
-        j++;
-    
+    while(left<m){
+        v.push_back(ar[left++]);
     }
-    while(i<=en1){
-        v.pb(ar[i]);
-        i++;
+    while(right<en){
+        v.push_back(ar[right++]);
     }
-    ll cnt=0;
-    for(int i=st1;i<=en1;i++){
-        ar[i]=v[cnt];cnt++;
-    }
-    for(int i=st2;i<=en2;i++){
-        ar[i]=v[cnt];cnt++;
+    for(int i=st;i<en;i++){
+        ar[i]=v[i-st];
     }
 }
 
-void merge_sort(std::vector<ll> &ar, ll st,ll en){
-    if(st>=en)return;
-    ll p=(st+en)/2;
-    merge_sort(ar,st,p);
-    merge_sort(ar,p+1,en);
-    merge(ar,st,p,p+1,en);
+void merge_sort(vector<int> & ar, int st, int en){
+    if(en-st<=1)return;
+    int m=(st+en)/2;
+    merge_sort(ar,st,m);
+    merge_sort(ar,m,en);
+    merge(ar, st,en);
 }
 
-void solve(){
-    ll n;
+void merge_sort(vector<int> & ar, int n){
+    merge_sort(ar,0,n);
+}
+
+int main(){ 
+    freopen("input.txt", "r", stdin);
+    int n;
     cin>>n;
-    std::vector<ll> ar(n);
-    input(ar,n);
-    merge_sort(ar,0,n-1);
-    for(auto it:ar)cout<<it<<" ";cout<<endl;
-}
+    vector<int> ar(n);
+    for(int id=0;id<n;id++){
+        cin>>ar[id];
+    }
+    display(ar);
+    merge_sort(ar,n);
+    display(ar);
 
-
-int main(){     
-    sub;    
-    fast;
-        int t=1;
-    // cin>>t;
-    while(t--){
-        solve();
-        }
     return 0;
-}        
-
-                                // Mischief Managed //    
+}

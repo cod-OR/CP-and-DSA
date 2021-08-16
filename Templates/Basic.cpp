a69e1d1ebbf2476258937173b51efb7b9199cdf9
Onkar Ratnaparkhi <73252373+cod-OR@users.noreply.github.com>
2021-08-16 13:21:07 +0530

    New basic template
    Added rng, comparator class, ONLINE_JUDGE flag, cin and cout for vectors and pairs

--- Templates/Basic.cpp
+++ Templates/Basic.cpp
@@ -1,16 +1,18 @@
 
+
                         //    I solemnly swear that I am upto no good //
 
 #include <bits/stdc++.h>
 using namespace std;
 #define sub             freopen("input.txt", "r", stdin);//freopen("output.txt", "w", stdout);
 #define ll              long long
+#define ull             unsigned long long
 #define ld              long double
-#define ttime           cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';
+#define ttime           {cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';}
+#define helpUs          template<typename T = ll , typename U = ll>
+#define helpMe          template<typename T = ll>
 #define pb              push_back
-#define unq(a)          sort(all(a));(a).resize(unique(all(a)) - (a).begin())
 #define sz(x)           (int)((x).size())
-#define input(arr,n)    for(ll i1=0;i1<n;i1++ )cin>>arr[i1]
 #define fast            ios_base::sync_with_stdio(false);cin.tie(0);
 #define all(x)          (x).begin(),(x).end()
 #define rep(i,a,b)      for(ll i=a;i<b;i++)
@@ -24,43 +26,50 @@ using namespace std;
 #define ppcll           __builtin_popcountll
 
 const long long INF=1e18;
-const long long A=200005;
-const long long mod=1000000007;                    // 2971215073, 1000050131, 433494437 (some random prime numbers)
+const long long N=200005;
+const long long mod=1000000007;                    // 998244353, 2971215073, 1000050131, 433494437
 
 #define endl "\n"
-// #define int ll
+#define int ll
 
 typedef pair<ll,ll> pairll;
 typedef map<ll,ll>  mapll;
 typedef map<char,ll> mapch;
-bool comp(ll a,ll b){
-    return a>b;
-}
+typedef vector<ll> vll;
+mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+helpUs class comp{public:bool operator()(T a, U b){return a>b;}};
+helpUs istream& operator>>(istream& aa, pair<T,U> &p){aa>>p.ff>>p.ss;return aa;}
+helpMe ostream& operator<<(ostream& ja, vector<T> &v){for(auto it:v)ja<<it<<" ";return ja;}
+helpMe istream& operator>>(istream& aa, vector<T> &v){for(auto &it:v)cin>>it;return aa;}
+helpUs ostream& operator<<(ostream& ja, pair<T,U> &p){ja<<p.ff<<" "<<p.ss;return ja;}
 
 ll n,k,tt=1;
 void solve(){
-    //string s,t;
-    //ll m,q,x,y,ans=0;
-    cin>>n; 
-    std::vector<ll> ar(n);
-    input(ar,n);
+
+    string s,t;
+    
+    cin>>n;
+    vll ar(n);
+    cin>>ar;
+    
     
 
 }
-
-signed main(){ 
-    sub;    
+           
+signed main(){
     fast;
-    // pre();
-    // clock_t clk = clock();
     ll t=1;
+    #ifndef ONLINE_JUDGE
+        sub;    
+    #endif
+    // clock_t clk = clock();
+    // pre();
+
     cin>>t;
-    while(t--){
+    while(t--)
         solve();
-    }
     // ttime;
     return 0;
 }        
 
-                                // Mischief Managed //   
-                                         
+                                // Mischief Managed //

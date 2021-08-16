a6ab1c30bbfa2a9995d64f84752c88ef36400c22
Onkar Ratnaparkhi <73252373+cod-OR@users.noreply.github.com>
2021-08-16 13:14:13 +0530

    Update Generator.cpp
    modification in accordance with new basic template

--- Templates/Generator.cpp
+++ Templates/Generator.cpp
@@ -1,27 +1,27 @@
-mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+// mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+// uncomment if not already present
 
+// const int N=1e5
 void solve(){
     freopen("input.txt", "w", stdout);   // Directs the output to file "input.txt"
     
     ll t=1;
-    t= rng()%N+1;         // Testcases
+    t= rng()%N+1;         // Testcases, if required
     cout<<t<<endl;
     while(t--){
-        
-        n= rng() % A +1 ;
+        n= rng() % N +1 ;
         cout<<n<<endl;
-
         std::vector<ll> ar(n);
-        
         for(int i=0;i<n;i++){
-            ar[i]= rng() % A + 1;
+            ar[i]= rng() % N + 1;
         }
 
         // to creat a permutation
         // for (int i = 0; i < N; i++)ar[i] = i;
         // shuffle(all(ar), rng);
 
-        for(auto it:ar)cout<<it<<" ";
+        for(auto it:ar)
+            cout<<it<<" ";
         cout<<endl;    
     }
 }

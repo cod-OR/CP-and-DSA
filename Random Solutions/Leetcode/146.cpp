795c9f2196bb8d8b528d9f2a0b9a1545b5ae6c92
Onkar Ratnaparkhi <73252373+cod-OR@users.noreply.github.com>
2021-11-27 00:01:49 +0530

    Update 146.cpp

--- Random Solutions/Leetcode/146.cpp
+++ Random Solutions/Leetcode/146.cpp
@@ -1,97 +1,109 @@
 
+
 // 146. LRU Cache
+
 
 class dllNode{
   public:
-    int key;
     int val;
-    dllNode* prev=NULL;
-    dllNode* next=NULL;
+    int key;
+    dllNode* prev = NULL;
+    dllNode* next = NULL;
 };
 
 class LRUCache {
     
-    dllNode* front, *rear;
-    int size;
+    dllNode* front=NULL,  *rear=NULL;
     int capacity;
+    int size;
     unordered_map<int, dllNode*> store;
-    
-    void insertNode(int key, int val){
-        if(size==capacity){
-            deleteNode(rear);
-            insertNode(key, val);
-            return;
-        }
-        
-        dllNode* node=new dllNode();
-        node->val=val;
-        node->key=key;
-        if(size==0){
-            front=rear=node;
-        }
-        else{
-            front->next=node;
-            node->prev=front;
-            front=node;
-        }
-        
-        store[key]=front;
-        size++;
-    }
     
     void deleteNode(dllNode* node){
         if(node->prev)
             node->prev->next = node->next;
         if(node->next)
             node->next->prev = node->prev;
-        
         if(node==rear)
-            rear=node->next;
-        if(node==front)
-            front=node->prev;
-        
-        store.erase(node->key);
-        delete node;
-        
+            rear = node->next;
+        if(node == front)
+            front = node->prev;
         size--;
+        delete node;
+    }
+    
+    void insertNode(int key, int val){
+        
+        if(size == capacity){
+            store.erase(rear->key);
+            deleteNode(rear);
+            insertNode(key,val);
+            return;
+        }
+        
+        dllNode* node = new dllNode();
+        node -> val = val;
+        node -> key = key;
+        if(size==0){
+            rear = front = node;
+        }
+        else{
+            front ->next = node;
+            node->prev = front;
+            front = node;
+        }
+        
+        size++;
     }
     
 public:
     LRUCache(int capacity) {
-        this->capacity = capacity;
+        this -> capacity = capacity;
+        rear=front=NULL;
         size=0;
-        front = rear = NULL;
     }
     
-    int get(int key) {
-        if(store.find(key) == store.end()){
-            return -1; // key not present in store
+    int get(int key){
+        int val;
+        
+        if(store.find(key) != store.end()){
+            
+            dllNode* node = store[key];
+            val = node->val;
+            
+            // remove from previous pos
+            store.erase(key);
+            deleteNode(node);
+            
+            // add at new pos
+            insertNode(key,val);
+            store[key] = front;
+            
         }
         else{
-            dllNode* node = store[key];
-            int val = node->val;
-            
-            // delete from current pos and attatch at the front
-            deleteNode(node);
-            insertNode(key, val);
-            
-            return val;
+           // not present in store
+           val = -1;
         }
+        
+        return val;
+        
     }
     
-    void put(int key, int value) {
+    void put(int key, int val) {
+
         if(store.find(key) != store.end()){
+            // if this key is present, remove it
             deleteNode(store[key]);
             store.erase(key);
         }
         
-        // add the key to store
-        insertNode(key, value);
-                
-        // after inserting, the position of this key is at front
+        // add at new position
+        insertNode(key , val);
         store[key]=front;
+        
     }
+  
 };
+
 
 /**
  * Your LRUCache object will be instantiated and called as such:

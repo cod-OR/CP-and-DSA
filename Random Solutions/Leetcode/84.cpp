67210971b98afda14b81f434428cddf15e6571d3
Onkar Ratnaparkhi <73252373+cod-OR@users.noreply.github.com>
2021-11-30 16:03:33 +0530

    Update 84.cpp

--- Random Solutions/Leetcode/84.cpp
+++ Random Solutions/Leetcode/84.cpp
@@ -1,6 +1,18 @@
 
 // 84. Largest Rectangle in Histogram
 
+// One possible approach is,
+// For each column, assume this column is going to be present in solution and see how much can it be extended towards left and right.
+// Doing this in brute force will require O(n) time for each column, So overall O(n squared)
+
+// Observe that:
+// The rectangle can be extended, only till the column which is to the right and whose height is smaller than current column, same towards left.
+// So we need to find the position of immediate left smaller element, And Same for right.
+// difference between them will be the width of rectangle. (height is equal to that of current column.)
+// O(n) time, O(n) space.
+
+// Implementation can further be simplified using a stack
+
 class Solution {
 public:
     int largestRectangleArea(vector<int>& h) {
